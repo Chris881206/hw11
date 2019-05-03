610a919e7fcb8ca487e8284db73f4d2a0853e3de
Chris881206 <48573768+Chris881206@users.noreply.github.com>
2019-05-03 15:46:19 +0800

    Add files via upload

---
+++ HW11.c
@@ -0,0 +1,27 @@
+    
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+int a,b,c;
+printf("叫块俱计\n");
+scanf("%d%d%d",&a,&b,&c);
+if (a>b)
+if(a>c)
+if(b>c)
+printf("计パㄌ=%d%d%d\n",c,b,a);
+else
+printf("计パㄌ=%d%d%d\n",b,c,a);
+else
+printf("计パㄌ=%d%d%d\n",b,a,c);
+else
+if (b>c)
+if (a>c)
+printf("计パㄌ=%d%d%d\n",c,a,b);
+else
+printf("计パㄌ=%d%d%d\n",a,c,b);
+else
+printf("计パㄌ=%d%d%d\n",a,b,c);
+system("PAUSE");
+return 0;
+}
